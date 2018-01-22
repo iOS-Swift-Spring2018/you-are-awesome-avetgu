11bad769fdd23486c321edef4dbf360f94ea37ad
Avery Gu <gua@bc.edu>
2018-01-22 17:19:24 -0500

    first commit

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
+    
+    //code below executes when app's view first loads
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("The View Loaded!")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("*** The button was clicked!") //use distinguishing characters to help debug more easily
+    
+//        let message1 = "You Are da Bomb"
+//        let message2 = "You Are Great"
+//        let message3 = "You Are Amazing"
+//
+//        if messageLabel.text == message1{
+//            messageLabel.text = message2
+//            messageLabel.textColor = UIColor.blue
+//        }
+//        else if messageLabel.text == message2{
+//            messageLabel.text = message3
+//            messageLabel.textColor = UIColor.red
+//        }
+//        else{
+//            messageLabel.text = message1
+//            messageLabel.textColor = UIColor.green
+//        }
+        
+        let messages = ["You Are Fantastic", "You Are Great",
+                        "You Are Amazing", "Genius Bar Needs You!",
+                        "You Brighten My Day!"]
+        
+        if index == 5{
+            index = 0
+        }
+        else{
+            messageLabel.text = messages[index]
+            index += 1
+        }
     }
-
-
 }
 
